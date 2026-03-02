80cc82a5057d21e8df2e580638ab88d086d7be50
Zhanina777 <eaa25zhsa@students.eaaa.dk>
2026-03-02 21:38:58 +0100

    doneeeeee

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,34 @@
-import './App.css'
-import Header from './components/Header';
-import Footer from './components/Footer';
-import { Routes, Route } from 'react-router-dom';
-import About from './views/About';
-import Plants from './views/Plants';
-import DefaultPage from './views/DefaultPage';
+//import './App.css'
+//import Header from './components/Header';
+//import Footer from './components/Footer';
+//import { Routes, Route } from 'react-router-dom';
+//import About from './views/About';
+//import Plants from './views/Plants';
+//import DefaultPage from './views/DefaultPage';
+
+//function App() {
+  //return (
+    //<>
+      //<Header />
+      //<div style={{ margin: '7rem 0 7rem 0' }}>
+        //<Routes>
+          //<Route path="/" element={<DefaultPage />} />
+          //<Route path="/plants" element={<Plants />} />
+          //<Route path="/about" element={<About />} />
+        //</Routes>
+      //</div>
+      //<Footer />
+    //</>
+  //);
+//}
+
+//export default App
+// in App.jsx or main.jsx
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
-    <>
+    <Router>
       <Header />
       <div style={{ margin: '7rem 0 7rem 0' }}>
         <Routes>
@@ -18,8 +38,8 @@ function App() {
         </Routes>
       </div>
       <Footer />
-    </>
+    </Router>
   );
 }
 
-export default App
+export default App;
